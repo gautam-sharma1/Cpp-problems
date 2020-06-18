3b1374d9c39e1eb31de262549f797e739323afc5
Gautam Sharma <55221455+gautam-sharma1@users.noreply.github.com>
2020-06-17 20:09:12 -0700

    basic function pointers

---
+++ function_pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void HelloWorld(int a)
+{
+    std::cout << "Hello World! Value: " << a << std::endl;
+}
+
+int main()
+{
+
+
+      typedef void(*HelloWorldFunction)(int);
+      
+      HelloWorldFunction function = HelloWorld;
+      
+      function(8);
+      
+
+
+
+}
